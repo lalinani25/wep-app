00baec3867a552f342481110a2d0942d68a738d2
Laura Alinani <lalinani@eagles.bridgewater.edu>
2024-02-14 20:24:47 -0500

    updated commit

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+async function fetchUsingDataFromForm() {
+    const mssg8 = document.querySelector("#message8")
+    const input = document.querySelector("#input8")
+    const url = "https://thankful-plant-0f567c30f.4.azurestaticapps.net/createAccount.html"
+    const data = {
+    code: input.value
+    }
+    const options = {
+    method: "POST",
+    headers: { "Content-Type": "createAccount/json" },
+    body: JSON.stringify(data)
+    }
+    let response = await fetch(url, options)
+    if (response.status == 200) {
+    const obj = await response.json()
+    mssg8.innerHTML = obj.message
+    }
+    else if (response.status == 401) {
+    const obj = await response.json()
+    mssg8.innerHTML = "Error: " + obj.message
+    }
+    else if (response.status == 400) {
+    mssg8.innerHTML = "Server error"
+    }
+   }
+   document.querySelector("#button8").addEventListener("click",
+   fetchUsingDataFromForm)
+   
